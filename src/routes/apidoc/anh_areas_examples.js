ee221924942a10b45b9318d226f415b6bc306f28
Erika Suárez Valencia <esuarez@humboldt.org.co>
2019-11-13 16:04:16 -0500

    Implement endpoint to list all anh areas names

---
+++ src/routes/apidoc/anh_areas_examples.js
@@ -0,0 +1,13 @@
+/**
+ * @apiDefine listAllAnhAreasSE
+ * @apiSuccessExample {json} Success-Example:
+ *  [
+ *    { "name": "CAG 7" },
+ *    { "name": "CAG 8" },
+ *    { "name": "CAT 4" },
+ *    { "name": "COR 9" },
+ *    { "name": "LLA 100" },
+ *    { "name": "LLA 101" },
+ *    { "name": "LLA 103" }
+ *  ]
+ */
